5133f1c41505d1dc3874b8727e10ebc30cef9abe
Miha Zidar <zidarsk8@gmail.com>
2016-05-27 11:10:59 +0200

    Version bump

--- wbpy/__init__.py
+++ wbpy/__init__.py
@@ -5,7 +5,7 @@ from wbpy.indicators import IndicatorAPI
 from wbpy.indicators import IndicatorDataset
 
 __name__ = "wbpy"
-__version__ = "2.0.1"
+__version__ = "2.1.1"
 __email__ = "matt@mattduck.com"
 __maintainer__ = "Matthew Duck"
 __license__ = "MIT"
